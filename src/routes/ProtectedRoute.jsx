1db4bf245a543085e385f8f32b9ac3e59411961f
Chinelo <onuegbuchinelom@gmail.com>
2026-08-25 03:13:10 -1100

    feat: Subscribe component created

--- src/routes/ProtectedRoute.jsx
+++ src/routes/ProtectedRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import Login from "../pages/Login";
 
 export default function ProtectedRoute(){
-    const isLoggedIn = false;
+    const isLoggedIn = true;
 
     if (!isLoggedIn) {
         return <Navigate to="/login" element={<Login />} /> // Redirect the user back to the login page
