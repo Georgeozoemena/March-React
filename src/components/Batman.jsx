b10b4cb71e131d2e55982bc3e464d3442e480cbc
Lucy <lucyanozie30@gmail.com>
2026-08-26 12:09:30 +0100

    flash is better

---
+++ src/components/Batman.jsx
@@ -0,0 +1,12 @@
+export default function Batman() {
+    return (
+        <div>
+            <h1>Batman</h1>
+            <p> Batman is the secret identity of Bruce Wayne, a wealthy American playboy, philanthropist, and owner of Wayne Enterprises. Batman should not sleep at night.
+                flash is better than Batman btw.
+                some people would beg to differ.
+                
+            </p>
+        </div>
+    );
+}
